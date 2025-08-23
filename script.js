4c009d7a60afdfea4594938c3358c3ea3f9ccbe0
Neya <75677598+NeyaTetsu@users.noreply.github.com>
2025-08-23 17:38:06 +0900

    Update UI
    大幅改良しました

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 function createBarcode() {
     const text = document.getElementById("inputText").value;
+    if (text.length == 0){
+        return;
+    }
     const lines = text.split(/\r?\n/).filter(line => line.trim() !== "");
 
-    let html = "<table><tbody>";
+    let html = '<table><tbody>';
 
     lines.forEach(line => {
         let barcodeNumber = line.replace(/[^0-9]/g, "");
         let tableHtml;
         if(barcodeNumber.length === 13){
             let barcodeImage = `<img src="https://inticket.sej.co.jp/order/barcode?Code=${barcodeNumber}" alt="${barcodeNumber}">`;
-            tableHtml = `${barcodeImage}<br>${barcodeNumber}<br><label for="note">メモ：</label><input type="text" name="note" size="24">`;
+            tableHtml = `${barcodeImage}<p>${barcodeNumber}</p><input type="text" name="note" placeholder="メモ"><input type="checkbox" id="check${barcodeNumber}"><label for="check${barcodeNumber}">バーコード読込済</label>`;
         }else{
-            tableHtml = "払込番号は13桁です！";
+            tableHtml = "<p>払込番号は13桁です！</p>";
         }
         html += `<tr><td>${tableHtml}</td></tr>`;
     });
 
-    html += "</tbody></table>";
+    html += '</tbody></table><div class="split"></div>';
 
     document.getElementById("tableContainer").innerHTML = html;
 }
